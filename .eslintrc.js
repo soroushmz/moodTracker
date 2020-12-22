bfac86b06e25b69791bfb9061a2cc92299871bf7
Soroush Mohammadzadeh <soroushmzy@gmail.com>
2020-12-22 19:36:33 +0330

    feat(app): add rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,18 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    quotes: ["error", "double"],
+    "react-native/no-inline-styles": 0,
+    "react-native/no-unused-styles": 1,
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "error",
+  },
+  env: {
+    "jest/globals": true,
+  },
+  globals: {
+    AbortController: true,
+  },
 };
